ea49b4b5beefd4e5fa28047f8146905773744698
Jason Weiser <jweiser@protonmail.com>
2020-05-03 14:13:10 +0000

    first commit

---
+++ twitter.py
@@ -0,0 +1,13 @@
+import dotenv
+import os
+
+dotenv.load_dotenv('.env')
+
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+print(consumer_key)
+print(access_token)
+print(type(consumer_key))

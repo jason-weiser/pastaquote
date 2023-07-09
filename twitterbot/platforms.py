4a0b0e049e2673e223362c6d4ba0db1d17a5bee8
Jason Weiser <jweiser@protonmail.com>
2023-07-09 23:28:03 +0000

    update with ini instead of .env

--- twitterbot/platforms.py
+++ twitterbot/platforms.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-import dotenv
+#import dotenv
+import configparser
 import os
 from mastodon import Mastodon
 from requests_oauthlib import OAuth1Session
@@ -7,7 +8,11 @@ from requests_oauthlib import OAuth1Session
 
 current_d = Path(__file__)
 parent_d = current_d.resolve().parents[1]
-dotenv.load_dotenv(os.path.join(parent_d, 'data/.env'), override=True)
+config = configparser.ConfigParser()
+config.read(os.path.join(parent_d, 'data/config.ini'))
+twitter_cred = config['TWITTER']
+mastodon_cred = config['MASTODON']
+#dotenv.load_dotenv(os.path.join(parent_d, 'data/config.ini'), override=True)
 
 
 
@@ -15,10 +20,10 @@ class Twitter:
     base_url = 'https://api.twitter.com/'
 
     def __init__(self):
-        self.consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
-        self.consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
-        self.access_token = os.getenv('TWITTER_ACCESS_TOKEN')
-        self.access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+        self.consumer_key = twitter_cred['TWITTER_CONSUMER_KEY']
+        self.consumer_secret = twitter_cred['TWITTER_CONSUMER_SECRET']
+        self.access_token = twitter_cred['TWITTER_ACCESS_TOKEN']
+        self.access_token_secret = twitter_cred['TWITTER_ACCESS_TOKEN_SECRET']
         self.session = OAuth1Session(self.consumer_key,
                         client_secret=self.consumer_secret,
                         resource_owner_key=self.access_token,
@@ -34,11 +39,10 @@ class Twitter:
         return resp.status_code
 
 class Masto:
-    base_url = os.getenv('MASTO_BASE_URL')
 
     def __init__(self):
-        self.masto = Mastodon(os.getenv('MASTO_ACCESS_TOKEN'),\
-            os.getenv('MASTO_BASE_URL'))
+        self.masto = Mastodon(mastodon_cred['MASTO_ACCESS_TOKEN'],\
+            os.getenv['MASTO_BASE_URL'])
 
     def tootit(self, status):
         return self.masto(status)

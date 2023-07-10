6f6713ae6c602e5d3abf8109692fef5c104924fd
Jason Weiser <jweiser@protonmail.com>
2023-07-10 01:48:18 +0000

    changed ini to yaml

--- twitterbot/bot.py
+++ twitterbot/bot.py
@@ -5,6 +5,7 @@ import pickle
 import os
 import sys
 import argparse
+import yaml
 import tweet_types
 from pathlib import Path
 from platforms import Twitter
@@ -41,7 +42,12 @@ logging.basicConfig(
 current_dir = Path(__file__)
 parent_dir = current_dir.resolve().parents[1]
 pickle_dir = os.path.join(parent_dir, "data/number.p")
-mode_file = os.path.join(parent_dir, "data/mode.p")
+
+## Load the config
+with open(os.path.join(parent_dir, 'data/config.yaml')) as config_yml:
+    config = yaml.safe_load(config_yml)
+options = config['SETUP']
+
 
 ##Define objects
 
@@ -55,42 +61,31 @@ def make_pickle():
     initial_num = 0
     pickle.dump(initial_num, open(pickle_dir,"wb"))
 
-def choose_mode():
-    mode_choice = input("Please choose the mode (1-random, 2-sequential): ")
-    choice_made = False
-    while not choice_made:
-        try:
-            if int(mode_choice) == 1 or int(mode_choice) == 2:
-                pickle.dump(int(mode_choice), open(mode_file, "wb"))
-                choice_made = True
-            else:
-                print("Please choose 1 or 2")
-        except:
-            print("Run again and choose an integer")
-
 def tweet_it():
-    w_mode = pickle.load(open(mode_file,"rb"))
-    if w_mode == 2:
+    if options['TYPE'] == "sequential":
         tweet = tweet_types.s_run(parent_dir)
         logging.info("Tweet attempted: {}.\nResponse: {}" \
                     .format(tweet.rstrip('\n'), twitter.post_tweet(tweet)))
         logging.info("Post attempted: {}.\nResponse: {}" \
                     .format(tweet.rstrip('\n'), masto.tootit(tweet)))
-    elif w_mode == 1:
+    elif options['TYPE'] == "random":
         tweet = tweet_types.r_run(parent_dir)
         logging.info("Post attempted: {}.\nResponse: {}" \
                     .format(tweet.rstrip('\n'), twitter.post_tweet(tweet)))
         logging.info("Post attempted: {}.\nResponse: {}" \
                     .format(tweet.rstrip('\n'), masto.tootit(tweet)))
+
 def main():
 #    runlist = RunList(parent_dir)
 #    twitter = Twitter()
     #ensure that there is a mode file or create one with argparse
     #also makes new json because you will have an incomplete file
     #when switching from random to sequential
-    if not os.path.isfile(mode_file) or args.mode:
-        choose_mode()
-        runlist.runit()
+    if options['TYPE'] != "sequential" and options['TYPE'] != "random":
+        print("""
+        You need to choose the order in which the list will run
+        Please edit the config file in ../data/config.yaml
+        """)
     #if there isn't a numbering file in place and you want sequential tweets
     #this makes a number file
     if not os.path.isfile(pickle_dir):

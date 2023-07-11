0417b13c6be87c76de67b229ec8435eee7a2162e
Jason Weiser <jweiser@protonmail.com>
2023-07-11 17:36:51 +0000

    further fixes of directories, removed mode pickle

--- twitterbot/tweet_types.py
+++ twitterbot/tweet_types.py
@@ -8,7 +8,7 @@ def r_run(parent_dir):
     with open(os.path.join(parent_dir, 'data/quotes.json'),"r") as json_file:
         data = json.load(json_file)
         n = random.randint(0,len(data)-1)
-        tweet = "{}\n\t--{}".format(data[n]['quote'],data[n]['author'])
+        tweet = "{}".format(data[n])
         working_data = data[:]
         working_data.pop(n)
 
@@ -24,7 +24,7 @@ def s_run(parent_dir):
     with open(os.path.join(parent_dir, 'data/quotes.json')) as json_file:
         data = json.load(json_file)
         n = pickle.load(open(os.path.join(parent_dir,'data/number.p'),"rb"))
-        tweet = "{}\n\t--{}".format(data[n]['quote'],data[n]['author'])
+        tweet = "{}".format(data[n])
         if n == len(data)-1:
             n = 0
         else:

5554085481de640f9b01fb0be35a38e967a9b2ae
Jason Weiser <jweiser@protonmail.com>
2023-07-16 14:19:44 +0000

    removed test folder

--- test/new_env.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('/home/jason/python/twitterbot/test/test.json','r') as settings_json_file:
-    settings = json.load(settings_json_file)
-    print(settings['mode'])
-    if settings['mode'] == 'random':
-        print("Oh so random")

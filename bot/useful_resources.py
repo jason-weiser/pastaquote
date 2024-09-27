ef3a2eae4f5863e0a053683f09254aa374cbc4ca
Jason Weiser <jweiser@protonmail.com>
2024-09-27 16:12:52 -0400

    simplified code with relative paths

--- bot/useful_resources.py
+++ bot/useful_resources.py
@@ -2,16 +2,10 @@ import requests
 import os
 import logging
 import yaml
-from pathlib import Path
-
-## Find parent directory
-running_file = Path(__file__)
-current_dir = running_file.resolve().parents[0]
-parent_dir = running_file.resolve().parents[1]
 
 def log_this(input_for_log):
     ## Logging
-    logging_file = os.path.join(parent_dir, 'data/bot.log')
+    logging_file = '../data/bot.log'
     if not os.path.isfile(logging_file):
         log_file = open(logging_file, 'x')
         log_file.close()
@@ -33,6 +27,6 @@ def connection_validator(url_to_check):
         return response.status_code
 
 def pull_config(section):
-    with open(os.path.join(current_dir,'config.yaml')) as config_yml:
+    with open("./config.yaml") as config_yml:
         config = yaml.safe_load(config_yml)
     return config[section]
